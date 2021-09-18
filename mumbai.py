c81c04c9f709819e5c0251a19e5a0cac61ba0e40
valueflowever <756162364@qq.com>
2021-09-18 21:53:28 +0800

    mumbai update

--- mumbai.py
+++ mumbai.py
@@ -4,19 +4,18 @@ import time
 from tools import get_config, headers, proxies
 
 config = get_config('config.ini')
-RECEIVE_ADDRESS = config['CONF']['RECEIVE_ADDRESS']
 
 
-def mumbai_claim():
+def mumbai_claim(receive_address: str):
     count = 0
     while count <= 3:
         url = 'https://api.faucet.matic.network/transferTokens'
-        data = {"network": "mumbai", "address": RECEIVE_ADDRESS, "token": "maticToken"}
+        data = {"network": "mumbai", "address": receive_address, "token": "maticToken"}
         resp = requests.post(url=url, headers=headers, data=json.dumps(data), proxies=proxies).content
         resp = resp.decode('utf-8')
         if 'hash' in json.dumps(resp):
-            print("success,claim 1 tmatic")
+            print(f"{receive_address} success,claim 1 tmatic")
             break
         if 'err' in json.dumps(resp):
             count += 1
-            time.sleep(10)
+            time.sleep(5)
