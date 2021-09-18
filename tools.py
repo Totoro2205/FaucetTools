b1290912ca5fff1afe45fde74c35d28dd86c8885
valueflowever <756162364@qq.com>
2021-09-18 21:53:44 +0800

    add post_form_headers

--- tools.py
+++ tools.py
@@ -5,6 +5,13 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
 }
 
+post_form_headers = {
+    'content-type': 'text/plain;charset=UTF-8',
+    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+    'origin': 'https://faucets.chain.link',
+    'referer': 'https://faucets.chain.link/kovan'
+}
+
 proxies = {
     'http': 'socks5://127.0.0.1:8080',
     'https': 'socks5://127.0.0.1:8080'
